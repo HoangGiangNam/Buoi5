e47f271526e1527f56d27538238bab412b02ccd6
Hoàng Giang Nam <giangnamhoang0@gmail.com>
2022-11-11 14:47:15 +0700

    add btbuoi5

--- src/BTBuoi5/Bai1.java
+++ src/BTBuoi5/Bai1.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 
 public class Bai1 {
     public static void main(String[] args) {
-//        int ngay, thang, nam;
-//        Scanner scanner = new Scanner(System.in);
-//        ngay = scanner.nextInt();
-//        thang = scanner.nextInt();
-//        nam = scanner.nextInt();
-//        if (ngay > 0 && ngay <= 31 && thang > 0 && thang <= 12 && nam > 1989 && nam <= 2020) {
-//            System.out.println(ngay + "/" + thang + "/" + nam);
-//
-//        } else {
-//            IllegalArgumentException ecx;
-//            System.out.println("Nhập sai dữ liệu");
-//        }
+       int ngay, thang, nam;
+        Scanner scanner = new Scanner(System.in);
+        ngay = scanner.nextInt();
+        thang = scanner.nextInt();
+        nam = scanner.nextInt();
+        if (ngay > 0 && ngay <= 31 && thang > 0 && thang <= 12 && nam > 1989 && nam <= 2020) {
+           System.out.println(ngay + "/" + thang + "/" + nam);
+
+       } else {
+          IllegalArgumentException ecx;
+            System.out.println("Nhập sai dữ liệu");
+      }
 
 
 
